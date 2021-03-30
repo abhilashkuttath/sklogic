60cdc0231fc1191b6b4791db88af51a0cb2131a0
Abhilash <abhilashkuttath@hotmail.com>
2021-03-30 14:25:31 +0530

    seesion filter dend parametr

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -2,6 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { ShopService } from './shop.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
+import { IBrand } from '../shared/models/brand';
+
 
 @Component({
   selector: 'app-shop',
@@ -10,15 +13,47 @@ import { IProduct } from '../shared/models/product';
 })
 export class ShopComponent implements OnInit {
   products: IProduct;
+  brands: IBrand[];
+  types: IType[];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  // getProducts(){
+  //   this.shopService.getProducts().subscribe({
+  //     next: (result: any) => {
+  //       this.products = result;
+  //     }
+  //   });
+  // }
+  getProducts(){
     this.shopService.getProducts().subscribe(response => {
       this.products = response;
     }, error => {
       console.log(error);
     });
   }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+    getTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      this.types = response;
+    }, error => {
+      console.log(error);
+    });
+
+    }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -2,7 +2,9 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +17,12 @@ export class ShopService {
     getProducts(){
     return this.http.get<IPagination>(this.baseUrl + 'products');
     }
+    getBrands(){
+      return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+    }
+   getTypes(){
+     return this.http.get<IType[]>(this.baseUrl + 'products/types');
+   }
 
 }
 
